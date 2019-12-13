21629e3612a55b11e16348a303a626b8cfbeb9e0
Andrew <andrewtuck2004@gmail.com>
2019-12-12 19:25:28 -0800

    first commit

--- old/mainHome.js
+++
@@ -1,7 +0,0 @@
-function goSign(){
-  window.location.replace("https://bruhchat.000webhostapp.com/home.html");
-}
-
-function goAccount(){
-  window.location.replace("https://bruhchat.000webhostapp.com/accountCreation.html")
-}
